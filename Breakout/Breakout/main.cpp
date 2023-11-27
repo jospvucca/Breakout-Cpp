9b107a11d398b62aa144ff06a88318dbe52b32f9
Josip Vučica <jv.vucica@gmail.com>
2023-11-27 23:55:33 +0100

    Added sdl2 (still needs adjusting for work env)

--- Breakout/Breakout/main.cpp
+++ Breakout/Breakout/main.cpp
@@ -5,9 +5,18 @@
 #include "XMLFiles.h"
 #include "LevelFactory.h"
 #include <iostream>
+#include <Sdl.h>
 
-int main()
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
 {
+
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
     std::cout << "File name: " + std::string(XMLFiles::LEVEL_ONE) << std::endl;
 
     int levelCounter = 1;
@@ -20,6 +29,8 @@ int main()
     std::unique_ptr<Level> currentLevel = LevelFactory::createLevelFromConfig(1, config);
     
     std::cout << "Hello World!\n";
+	//SDL_QUIT();
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -32,3 +43,62 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+
+/*
+
+Using SDL and standard IO
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		//Create window
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface(window);
+
+			//Fill the surface white
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+			//Update the surface
+			SDL_UpdateWindowSurface(window);
+
+			//Hack to get window to stay up
+			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }
+		}
+	}
+
+	//Destroy window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+	return 0;
+}
+
+*/
